5ac2979e52668cd4ff7c18af8173dffccbefa048
Sean Wedig <sean.wedig@teamnorthwoods.com>
2018-10-10 00:23:22 -0400

    First pass at random ship addition
    It's not fully random, but it's good enough to make sure we don't
    overlap with otherships or the edge of the board.

--- players/sean_bot_player.rb
+++ players/sean_bot_player.rb
@@ -1,6 +1,9 @@
 class SeanBotPlayer 
+  attr_accessor :open_spaces
+
   def initialize
     @plays = 10.times.map { |x| 10.times.map { |y| [x, y] } }.flatten(1)
+    @open_spaces = 10.times.map { |y| 10.times.map { |x| [x, y] } }.flatten(1)
   end
 
   def name
@@ -8,16 +11,44 @@ class SeanBotPlayer
   end
 
   def new_game
-    [
-      [rand(6), 0, 5, :across],
-      [rand(7), 1, 4, :across],
-      [rand(8), 2, 3, :across],
-      [rand(8), 3, 3, :across],
-      [rand(9), 4, 2, :across]
-    ]
+    carrier = add_ship(5, :across)
+    titular_ship = add_ship(4, :down)
+    sub = add_ship(3, :across)
+    other_three_ship = add_ship(3, :down)
+    tiny_fucker = add_ship(2, :across)
+
+    [ carrier, titular_ship, sub, other_three_ship, tiny_fucker ]
   end
 
   def take_turn(state, ships_remaining)
     @plays.delete_at(rand(@plays.length))
+  end
+
+  def add_ship(size, orientation)
+    top_left = nil
+    cells = nil
+    loop do
+      top_left = @open_spaces[rand(@open_spaces.length)]
+
+      if (orientation == :across)
+        cells = (0..size-1).map { |x| [top_left.first + x, top_left.last] }
+      else
+        cells = (0..size-1).map { |x| [top_left.first, top_left.last + x] }
+      end
+
+      break if cells_available?(cells)
+    end
+
+    remove_available_cells(cells)
+
+    return top_left + [size, orientation]
+  end
+
+  def cells_available?(cells)
+    (cells - @open_spaces).empty?
+  end
+
+  def remove_available_cells(cells)
+    @open_spaces.reject! { |x| cells.include?(x) }
   end
 end
--- players/spec/sean_bot_player_spec.rb
+++ players/spec/sean_bot_player_spec.rb
@@ -14,4 +14,46 @@ describe SeanBotPlayer do
       expect(lengths).to match_array [2, 3, 3, 4, 5]
     end
   end
+
+  describe '#add_ship' do
+    it 'returns the ship structure' do
+      allow(subject).to receive(:rand).and_return 2
+
+      expect(subject.add_ship(2, :down)).to eq [2, 0, 2, :down]
+    end
+
+    it 'removes cells from the board model' do
+      subject.add_ship(5, :across)
+
+      expect(subject.open_spaces.length).to be 95
+    end
+
+    it 'uses a random top-left starting point' do
+      allow(subject).to receive(:rand).and_return 7
+
+      expect(subject.add_ship(2, :down)).to eq [7, 0, 2, :down]
+    end
+
+    it 'keeps selecting locations until one will not go off the bottom of the board' do
+      allow(subject).to receive(:rand).and_return(99, 99, 0)
+
+      expect(subject.add_ship(2, :down)).to eq [0, 0, 2, :down]
+    end
+
+    it 'keeps selecting locations until one will not go off the edge of the board' do
+      allow(subject).to receive(:rand).and_return(9, 19, 0)
+
+      expect(subject.add_ship(2, :across)).to eq [0, 0, 2, :across]
+    end
+
+    it 'keeps selecting locations until it does not overlap with other ships' do
+      allow(subject).to receive(:rand).and_return(1, 0, 0, 6)
+
+      subject.add_ship(5, :across) # 5 long at [1, 0]
+      final_coords = subject.add_ship(4, :across) # 4 long at [0, 0], [0, 0], then [1, 1]
+                                                  # (not [6, 0], because those cells are removed)
+
+      expect(final_coords).to eq [1, 1, 4, :across]
+    end
+  end
 end
